03270115bb7c08a01f3d9d53e674208723dae8dd
itati01 <itati01@users.noreply.github.com>
2019-02-19 15:35:11 +0100

    implement transport efficiency in flow accumulation (#70)
    efficiency reduces the outflow of a raster cell
    values can be percentage or relative

--- pysheds/grid.py
+++ pysheds/grid.py
@@ -1103,7 +1103,7 @@ class Grid(object):
     #     grid_props = self._generate_grid_props(**private_props)
     #     return self._output_handler(result, inplace, out_name=out_name, **grid_props)
 
-    def accumulation(self, data, weights=None, dirmap=None, nodata_in=None, nodata_out=0,
+    def accumulation(self, data, weights=None, dirmap=None, nodata_in=None, nodata_out=0, efficiency=None,
                      out_name='acc', routing='d8', inplace=True, pad=False, apply_mask=False,
                      ignore_metadata=False, **kwargs):
         """
@@ -1123,6 +1123,9 @@ class Grid(object):
                  List of integer values representing the following
                  cardinal and intercardinal directions (in order):
                  [N, NE, E, SE, S, SW, W, NW]
+        efficiency: numpy ndarray
+                 Array of relative efficiency factors (0..1) or percentages (0..100)
+                 to be applied to each accumulation cell. Must be same size as data.
         nodata_in : int or float
                     Value to indicate nodata in input array. If using a named dataset, will
                     default to the 'nodata' value of the named dataset. If using an ndarray,
@@ -1155,19 +1158,19 @@ class Grid(object):
                                    properties=properties,
                                    ignore_metadata=ignore_metadata, **kwargs)
         if routing.lower() == 'd8':
-            return self._d8_accumulation(fdir=fdir, weights=weights, dirmap=dirmap,
+            return self._d8_accumulation(fdir=fdir, weights=weights, dirmap=dirmap, efficiency=efficiency,
                                          nodata_in=nodata_in, nodata_out=nodata_out,
                                          out_name=out_name, inplace=inplace, pad=pad,
                                          apply_mask=apply_mask, ignore_metadata=ignore_metadata,
                                          properties=properties, metadata=metadata, **kwargs)
         elif routing.lower() == 'dinf':
-            return self._dinf_accumulation(fdir=fdir, weights=weights, dirmap=dirmap,
+            return self._dinf_accumulation(fdir=fdir, weights=weights, dirmap=dirmap,efficiency=efficiency,
                                            nodata_in=nodata_in, nodata_out=nodata_out,
                                            out_name=out_name, inplace=inplace, pad=pad,
                                            apply_mask=apply_mask, ignore_metadata=ignore_metadata,
                                            properties=properties, metadata=metadata, **kwargs)
 
-    def _d8_accumulation(self, fdir=None, weights=None, dirmap=None, nodata_in=None, nodata_out=0,
+    def _d8_accumulation(self, fdir=None, weights=None, dirmap=None, nodata_in=None, nodata_out=0,efficiency=None,
                          out_name='acc', inplace=True, pad=False, apply_mask=False,
                          ignore_metadata=False, properties={}, metadata={}, **kwargs):
         # Pad the rim
@@ -1203,20 +1206,45 @@ class Grid(object):
                 acc = weights.flatten()
             else:
                 acc = (~nodata_cells).ravel().astype(int)
-            # TODO: Does this need to have a min length?
+            # efficiency either percentage or relative
+            # percentage converted to relative values
+            if efficiency is not None:
+                assert(efficiency.size == fdir.size)
+                eff_max, eff_min = np.max(efficiency), np.min(efficiency)
+                if eff_max>1:       # percent?
+                    assert((eff_max<=100) and (eff_min>=0))
+                    # TODO: flatten to prevent original values from being modified?
+                    efficiency = efficiency.flatten() / 100.
+                else:
+                    assert((eff_max<=1) and (eff_min>=0))
+            #else:
+            #    efficiency = np.ones_like(acc)
             indegree = np.bincount(endnodes)
             indegree = indegree.reshape(acc.shape).astype(np.uint8)
             startnodes = startnodes[(indegree == 0)]
             endnodes = fdir.flat[startnodes]
-            for _ in range(fdir.size):
-                if endnodes.any():
-                    np.add.at(acc, endnodes, acc[startnodes])
-                    np.subtract.at(indegree, endnodes, 1)
-                    startnodes = np.unique(endnodes)
-                    startnodes = startnodes[indegree[startnodes] == 0]
-                    endnodes = fdir.flat[startnodes]
-                else:
-                    break
+            # separate for loop to avoid performance hit when
+            # efficiency is None
+            if efficiency is None: # no efficiency
+                for _ in range(fdir.size):
+                    if endnodes.any():
+                        np.add.at(acc, endnodes, acc[startnodes])
+                        np.subtract.at(indegree, endnodes, 1)
+                        startnodes = np.unique(endnodes)
+                        startnodes = startnodes[indegree[startnodes] == 0]
+                        endnodes = fdir.flat[startnodes]
+                    else:
+                        break
+            else:               # apply efficiency
+                for _ in range(fdir.size):
+                    if endnodes.any():
+                        np.add.at(acc, endnodes, acc[startnodes] * efficiency[startnodes])
+                        np.subtract.at(indegree, endnodes, 1)
+                        startnodes = np.unique(endnodes)
+                        startnodes = startnodes[indegree[startnodes] == 0]
+                        endnodes = fdir.flat[startnodes]
+                    else:
+                        break
             # TODO: Hacky: should probably fix this
             acc[0] = 1
             # Reshape and offset accumulation
@@ -1239,7 +1267,7 @@ class Grid(object):
         return self._output_handler(data=acc, out_name=out_name, properties=properties,
                                     inplace=inplace, metadata=metadata)
 
-    def _dinf_accumulation(self, fdir=None, weights=None, dirmap=None, nodata_in=None, nodata_out=0,
+    def _dinf_accumulation(self, fdir=None, weights=None, dirmap=None, nodata_in=None, nodata_out=0,efficiency=None,
                            out_name='acc', inplace=True, pad=False, apply_mask=False,
                            ignore_metadata=False, properties={}, metadata={}, **kwargs):
         # Filter warnings due to invalid values
@@ -1282,6 +1310,20 @@ class Grid(object):
                 acc = weights.flatten().astype(float)
             else:
                 acc = (~nodata_cells).ravel().astype(float)
+            # efficiency either percentage or relative
+            # percentage converted to relative values
+            if efficiency is not None:
+                assert(efficiency.size == fdir.size)
+                eff_max, eff_min = np.max(efficiency), np.min(efficiency)
+                if eff_max>1:       # percent?
+                    assert((eff_max<=100) and (eff_min>=0))
+                    # TODO: flatten to prevent original values from being modified?
+                    efficiency = efficiency.flatten() / 100.
+                else:
+                    assert((eff_max<=1) and (eff_min>=0))
+            #else:
+            #    efficiency = np.ones_like(acc)
+            
             # Ensure no flow directions with zero proportion
             fdir_0.flat[prop_0 == 0] = fdir_1.flat[prop_0 == 0]
             fdir_1.flat[prop_1 == 0] = fdir_0.flat[prop_1 == 0]
@@ -1306,25 +1348,46 @@ class Grid(object):
             endnodes_0 = fdir_0.flat[startnodes]
             endnodes_1 = fdir_1.flat[startnodes]
             epsilon = 1e-8
-            for _ in range(fdir.size):
-                if (startnodes.any()):
-                    np.add.at(acc_i, endnodes_0, prop_0[startnodes]*acc[startnodes])
-                    np.add.at(acc_i, endnodes_1, prop_1[startnodes]*acc[startnodes])
-                    acc += acc_i
-                    acc_i.fill(0)
-                    np.subtract.at(indegree, endnodes_0, prop_0[startnodes])
-                    np.subtract.at(indegree, endnodes_1, prop_1[startnodes])
-                    startnodes = np.unique(np.concatenate([endnodes_0, endnodes_1]))
-                    startnodes = startnodes[np.abs(indegree[startnodes]) < epsilon]
-                    endnodes_0 = fdir_0.flat[startnodes]
-                    endnodes_1 = fdir_1.flat[startnodes]
-                    # TODO: This part is kind of gross
-                    startnodes = startnodes[~((startnodes == endnodes_0) &
-                                              (startnodes == endnodes_1))]
-                    endnodes_0 = fdir_0.flat[startnodes]
-                    endnodes_1 = fdir_1.flat[startnodes]
-                else:
-                    break
+            if efficiency is None:
+                for _ in range(fdir.size):
+                    if (startnodes.any()):
+                        np.add.at(acc_i, endnodes_0, prop_0[startnodes]*acc[startnodes])
+                        np.add.at(acc_i, endnodes_1, prop_1[startnodes]*acc[startnodes])
+                        acc += acc_i
+                        acc_i.fill(0)
+                        np.subtract.at(indegree, endnodes_0, prop_0[startnodes])
+                        np.subtract.at(indegree, endnodes_1, prop_1[startnodes])
+                        startnodes = np.unique(np.concatenate([endnodes_0, endnodes_1]))
+                        startnodes = startnodes[np.abs(indegree[startnodes]) < epsilon]
+                        endnodes_0 = fdir_0.flat[startnodes]
+                        endnodes_1 = fdir_1.flat[startnodes]
+                        # TODO: This part is kind of gross
+                        startnodes = startnodes[~((startnodes == endnodes_0) &
+                                                  (startnodes == endnodes_1))]
+                        endnodes_0 = fdir_0.flat[startnodes]
+                        endnodes_1 = fdir_1.flat[startnodes]
+                    else:
+                        break
+            else:
+                for _ in range(fdir.size):
+                    if (startnodes.any()):
+                        np.add.at(acc_i, endnodes_0, prop_0[startnodes]*acc[startnodes] * efficiency[startnodes])
+                        np.add.at(acc_i, endnodes_1, prop_1[startnodes]*acc[startnodes] * efficiency[startnodes])
+                        acc += acc_i
+                        acc_i.fill(0)
+                        np.subtract.at(indegree, endnodes_0, prop_0[startnodes])
+                        np.subtract.at(indegree, endnodes_1, prop_1[startnodes])
+                        startnodes = np.unique(np.concatenate([endnodes_0, endnodes_1]))
+                        startnodes = startnodes[np.abs(indegree[startnodes]) < epsilon]
+                        endnodes_0 = fdir_0.flat[startnodes]
+                        endnodes_1 = fdir_1.flat[startnodes]
+                        # TODO: This part is kind of gross
+                        startnodes = startnodes[~((startnodes == endnodes_0) &
+                                                  (startnodes == endnodes_1))]
+                        endnodes_0 = fdir_0.flat[startnodes]
+                        endnodes_1 = fdir_1.flat[startnodes]
+                    else:
+                        break
             # TODO: Hacky: should probably fix this
             acc[0] = 1
             # Reshape and offset accumulation
